26263a4068e54c98b80de01fb24a7434ddfc85d8
Peter Grimtell <peter.grimtell@bredband.net>
2016-06-20 18:43:46 +0000

    Improved 1.1, 1.2. Solved 1.3.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,27 @@
 # Part 1
 
 def sum arr
-  arr.inject(0){|mysum,x| mysum + x }
+  #arr.inject(0){|mysum,x| mysum + x }
+  arr.reduce(0, :+)
 
 end
 
 
 def max_2_sum arr
-  sum(arr.sort {|a,b| b <=> a}.take(2))
+  #sum(arr.sort {|a,b| b <=> a}.take(2))
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-
-    arr.index {|x| x == n}
-    #   return true
-    # end
-    
-     arr.contains? do |y|
-       y == n - x
-     end
-   end
+    # Insert all array elements in hash, with a count for each unique element
+    h = Hash.new(0)
+    arr.each {|e| h[e] += 1 }
+    # Check if there is any element e in the array such that the sum of that
+    # element and another is n, by looking in the hash for (n-e). If 2*e==n,
+    # then the count (hash value) must be at least 2 since we must add two
+    # different elements.
+    arr.any? {|e| h[n-e] >= ((2*e==n)?2:1) }
+end
 
 #end
 
